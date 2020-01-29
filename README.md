f3abca8c0f21872a76b38ea74db922c745bb2bde
Morgan Benton <registered@morphatic.com>
2020-01-29 14:11:40 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FizzBuzz2020
+
+A demo of how to create a git repo from the command line
---
+++ __tests__/index.js
@@ -0,0 +1,5 @@
+describe('A smoke test', () => {
+  it('will let us confirm our test infrastructure is setup correctly', () => {
+    expect(true).toBe(true)
+  })
+})
